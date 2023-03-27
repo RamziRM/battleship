0307533882e8162432c302723b50ca3e141e50bc
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-03-27 12:48:26 -0300

    Ship factory f()

--- src/factories/ship.js
+++ src/factories/ship.js
@@ -1,2 +1,20 @@
 // Ship factory function
 // ship type, length, number of hits, isSunk status
+
+function Ship(type, length) {
+  let hits = 0;
+  let isSunk = false;
+
+  function hit() {
+    hits++;
+    if (hits === length) {
+      isSunk = true;
+    }
+  }
+
+  function getIsSunk() {
+    return isSunk;
+  }
+
+  return { type, length, hit, getIsSunk };
+}
