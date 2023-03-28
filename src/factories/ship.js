f77d961b9396311ebd5f190e044f102977a0b01b
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-03-28 10:50:04 -0300

    adjust Ship factory

--- src/factories/ship.js
+++ src/factories/ship.js
@@ -1,20 +1,26 @@
 // Ship factory function
-// ship type, length, number of hits, isSunk status
+// ship type?, length, number of hits, isSunk status
 
-function Ship(type, length) {
-  let hits = 0;
-  let isSunk = false;
+function Ship(length) {
+    let hits = 0;
+    let isSunk = false;
 
-  function hit() {
+    function hit() {
     hits++;
     if (hits === length) {
-      isSunk = true;
+        isSunk = true;
     }
-  }
+    }
 
-  function getIsSunk() {
+    function getIsSunk() {
     return isSunk;
-  }
+    }
 
-  return { type, length, hit, getIsSunk };
+    return { 
+        length, 
+        hit, 
+        getIsSunk
+    };
 }
+
+module.exports = Ship;
