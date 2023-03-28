2f5fef2b7292bcb97552da231c3e12163344e9ef
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-03-28 17:26:36 -0300

    player actions + random actions for comp

---
+++ src/player.js
@@ -0,0 +1,54 @@
+const gameboard = require('./gameboard');
+
+function Player() {
+    const ownBoard = gameboard();
+    const enemyBoard = gameboard();
+
+    function attack(x, y, enemyBoard) {
+        return enemyBoard.receiveAttack(x, y);
+    }
+  
+    function randomAttack(pToAttack) {
+      let x = Math.floor(Math.random() * 10);
+      let y = Math.floor(Math.random() * 10);
+      while (missedAttacks.has(`${x},${y}`)) {
+        x = Math.floor(Math.random() * 10);
+        y = Math.floor(Math.random() * 10);
+      }
+      return [x, y];
+    }
+
+    // place ships randomly - within bounds and not overlapping using isValidPlacement function
+    function placeShipsRandomly() {
+        ownBoard.ships.forEach(ship => {
+            let x = Math.floor(Math.random() * 10);
+            let y = Math.floor(Math.random() * 10);
+            let isHorizontal = Math.random() < 0.5;
+            while (!ownBoard.isValidPlacement(ship, x, y, isHorizontal)) {
+                x = Math.floor(Math.random() * 10);
+                y = Math.floor(Math.random() * 10);
+                isHorizontal = Math.random() < 0.5;
+            }
+            ownBoard.placeShip(ship, x, y, isHorizontal);
+        });
+    }
+
+    function getOwnBoard() {
+        return ownBoard;
+    }
+
+    function getEnemyBoard() {
+        return enemyBoard;
+    }
+
+    return { 
+        attack,
+        randomAttack,
+        placeShipsRandomly, 
+        getOwnBoard, 
+        getEnemyBoard 
+    };
+
+}
+
+module.exports = Player;
