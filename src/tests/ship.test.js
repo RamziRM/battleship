1c29bccedd8b86bb9e8708fad5b5401a76ceb0a2
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-03-30 15:44:46 -0300

    refactor test - specific task

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,18 +1,30 @@
 // import ship factory function
 const Ship = require('../factories/ship');
 
+beforeEach(() => {
+    testShip = Ship(4);
+})
+
 // Ship factory function tests
-test('Ship factory function exists', () => {
-    expect(Ship).toBeDefined();
+
+// test if ship registers hits - adding a hit increases the number of hits
+test('Ship can register 1 hit', () => {
+    testShip.hit();
+    expect(testShip.getHits()).toBe(1);
 }
 );
 
-test('Ship factory function returns an object', () => {
-    expect(typeof Ship()).toBe('object');
-}
-);
+test('register 3 hits to testShip', () => {
+    testShip.hit();
+    testShip.hit();
+    testShip.hit();
+    expect(testShip.getHits()).toBe(3);
+});
 
-test('Ship factory function returns an object with a length property', () => {
-    expect(Ship()).toHaveProperty('length');
-}
-);
+
+test('Can testShip sink -- update isSunk property', () => {
+    for (let i = 0; i < 4; i++) {
+        testShip.hit();
+    }
+    expect(testShip.isSunk()).toBe(true);
+});
