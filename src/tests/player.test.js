734156fd609c555df08f8dedb9a0a282dfad17af
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-04-01 23:50:12 -0300

    test 2 random placement

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -15,3 +15,17 @@ test('player can recieve attack', () => {
     enemyPlayer.attack([3, 4], testPlayer);
     expect(testPlayer.ownBoard.hasAttack([3, 4])).toBe(true);
 });
+
+// test if can make random attack - loop through board and check if any of the squares have been attacked - expect 1 hit only
+test('player can make random attack', () => {
+    enemyPlayer.randomAttack(testPlayer);
+    let hasAttack = 0;
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            if (testPlayer.ownBoard.hasAttack([i, j])) {
+                hasAttack++;
+            }
+        }
+    }
+    expect(hasAttack).toBe(1);
+});
