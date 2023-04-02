77cf7a6f38e5f59cfc76704384da7bd0ff7eeca1
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-04-01 23:27:19 -0300

    first test p

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,3 +1,17 @@
 const Player = require('../player');
+const Gameboard = require('../factories/gameboard');
 
-// Test 1: 
+
+let testPlayer;
+let enemyPlayer;
+beforeEach(() => {
+    testPlayer = Player();
+    enemyPlayer = Player();
+});
+
+// test if player can recieve attack
+test('player can recieve attack', () => {
+    testPlayer.ownBoard.placeShip([3, 4], 2, true);
+    enemyPlayer.attack([3, 4], testPlayer);
+    expect(testPlayer.ownBoard.hasAttack([3, 4])).toBe(true);
+});
